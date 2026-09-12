5415f4f9d2a4a0c62c9e8466bbcd17b961ac77d8
Arya Dwi Putra <aryaadwptr@gmail.com>
2026-09-12 11:06:13 +0700

    fix: submit setup wizard after applying transform

--- resources/js/Pages/Setup/Wizard.jsx
+++ resources/js/Pages/Setup/Wizard.jsx
@@ -116,8 +116,8 @@ export default function Wizard({ businessTypes }) {
                           )
                         : category;
                 }),
-            }))
-            .post(route("setup.store"));
+            }));
+        post(route("setup.store"));
     };
 
     const selectedType = businessTypes?.find(
